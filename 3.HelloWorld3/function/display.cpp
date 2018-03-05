bc8501e98850ed0fd334fc0fda65abaed6f9c4da
XierHacker <xierhacker@163.com>
2018-03-05 17:19:23 +0800

    update

---
+++ 3.HelloWorld3/function/display.cpp
@@ -0,0 +1,11 @@
+//
+// Created by xierh on 2018/3/5.
+//
+
+#include <iostream>
+#include "display.h"
+
+void show()
+{
+    std::cout<<"Hello,World!"<<std::endl;
+}
---
+++ 3.HelloWorld3/function/display.h
@@ -0,0 +1,10 @@
+//
+// Created by xierh on 2018/3/5.
+//
+
+#ifndef INC_3_HELLOWORLD3_DISPLAY_H
+#define INC_3_HELLOWORLD3_DISPLAY_H
+
+void show();
+
+#endif //INC_3_HELLOWORLD3_DISPLAY_H
