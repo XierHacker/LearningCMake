be31ba3b42ca1d6abdfc0678a8db9047e847eb66
xierhacker <xierhacker@163.com>
2018-07-03 17:05:26 +0800

    add DLL

---
+++ 6.LinkLib/dynamic/compare.cpp
@@ -0,0 +1,9 @@
+//
+// Created by xierhacker on 18-7-3.
+//
+
+#include "compare.h"
+bool compare(int a,int b)
+{
+    return a>b;
+}
---
+++ 6.LinkLib/dynamic/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by xierhacker on 18-7-3.
+//
+
+#include <iostream>
+#include "compare.h"
+
+int main()
+{
+    int a=30,b=20;
+    std::cout<<compare(a,b)<<std::endl;
+    return 0;
+}
